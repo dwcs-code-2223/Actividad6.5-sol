ac093b7e2b04d4beb4a05534d61adb13dd99714e
mfernandez <mfernandez@W10N-I2E31.ateis.local>
2023-04-17 19:21:35 +0200

    A6.5 modal start

--- js/global.js
+++ js/global.js
@@ -12,8 +12,17 @@ function onceLoaded() {
 
     console.debug("window loaded");
     document.querySelector('#formLogin').onsubmit = loginJSON;
-    document.querySelector('#formLogout').onsubmit = logout;
-    
+    document.querySelector('#formLogout').onsubmit = function (event) {
+        event.preventDefault();
+        showModal('spa_modal', 'Confirmación',
+                '¿Está seguro de que desea cerrar sesión?',
+                'Sí', null, logout, null);
+    };
     getRoles();
+}
+function showModal(modal_id, title, msg, opt_ok_text = null,
+        opt_cancel_text = null, opt_ok_function = null, opt_cancel_function = null) {
+    var myModal = new bootstrap.Modal(document.getElementById(modal_id));
+    myModal.show();
 }
 
